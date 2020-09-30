c576303d10e0a19a7816495eaf3051b9262e1304
ijayasurya <67519756+ijayasurya@users.noreply.github.com>
2020-09-30 16:54:29 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+document.write( '<b:section class=\'main\' id=\'main\' showaddelement=\'yes\'>\n' );
+document.write( '<b:widget id=\'Blog1\' locked=\'false\' title=\'Blog Posts\' type=\'Blog\' version=\'1\'/>\n' );
+document.write( '</b:section>' );
